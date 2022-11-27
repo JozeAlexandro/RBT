56c30749858a00d54a610746a36d631e65a28a89
JozeAlexandro <petrov.sanya1998@yandex.ru>
2022-11-27 21:11:50 +0300

    Initial Commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "btType.h"
+
+
+
+
+
+/*
+//default: assert(false)
+#define cbrt(X) _Generic( (X), \
+              sdf: "s1", \
+              eed: "s2"  \
+            )
+*/
+
+
+int main(void)
+{
+
+
+}
